abcf28759ac317c33333ba58b351296c0f122e22
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-03-24 14:49:20 +0100

    Sync with mdformat 0.6.x (#2)

--- mdformat_gfm/plugin.py
+++ mdformat_gfm/plugin.py
@@ -1,10 +1,10 @@
 import re
-from typing import Any, Mapping, Optional, Sequence, Tuple
+from typing import Any, Mapping, MutableMapping
 
 from markdown_it import MarkdownIt
-from markdown_it.token import Token
 import mdformat.plugins
-from mdformat.renderer import MARKERS, MDRenderer
+from mdformat.renderer import DEFAULT_RENDERER_FUNCS, RenderTreeNode
+from mdformat.renderer.typing import RendererFunc
 from mdit_py_plugins.tasklists import tasklists_plugin
 
 
@@ -22,66 +22,46 @@ def update_mdit(mdit: MarkdownIt) -> None:
     mdit.use(tasklists_plugin)
 
 
-def render_token(
-    renderer: MDRenderer,
-    tokens: Sequence[Token],
-    index: int,
+def _strikethrough_renderer(
+    node: RenderTreeNode,
+    renderer_funcs: Mapping[str, RendererFunc],
     options: Mapping[str, Any],
-    env: dict,
-) -> Optional[Tuple[str, int]]:
-    token = tokens[index]
-
-    # Prevent endless recursion. Make sure this plugin never processes
-    # a token more than once.
-    if "mdformat-gfm-processed-tokens" not in env:
-        env["mdformat-gfm-processed-tokens"] = set()
-    if id(token) in env["mdformat-gfm-processed-tokens"]:
-        return None
-    env["mdformat-gfm-processed-tokens"].add(id(token))
-
-    # Render strikethroughs
-    if token.type == "s_open":
-        closing_index = _index_closing_token(tokens, index)
-        text = renderer.render(
-            tokens, options, env, start=index + 1, stop=closing_index, finalize=False
-        )
-        text = "~~" + text + "~~"
-        return text, closing_index
-
-    # Render task lists items
-    if token.type == "list_item_open":
-        classes = token.attrGet("class")
-        if classes is None or "task-list-item" not in classes:
-            return None
-        closing_index = _index_closing_token(tokens, index)
-
-        # Tasklists extension makes a bit weird token stream where
-        # tasks are annotated by html. We need to remove the HTML.
-        inline_token = tokens[index + 2]
-        assert inline_token.type == "inline"
-        assert (
-            inline_token.children is not None
-        ), "inline token's children must not be None"
-        html_inline = inline_token.children[0]
-        assert 'class="task-list-item-checkbox"' in html_inline.content
-        inline_token.children.remove(html_inline)
-        checkmark = "x" if 'checked="checked"' in html_inline.content else " "
-
-        text = renderer.render(
-            tokens, options, env, start=index, stop=closing_index + 1, finalize=False
-        )
-
-        text = re.sub(fr"{MARKERS.LIST_INDENT_FIRST_LINE}\s+", f" [{checkmark}] ", text)
-        return text, closing_index
-
-    return None
+    env: MutableMapping,
+) -> str:
+    content = "".join(
+        child.render(renderer_funcs, options, env) for child in node.children
+    )
+    return "~~" + content + "~~"
 
 
-def _index_closing_token(tokens: Sequence[Token], opening_index: int) -> int:
-    opening_token = tokens[opening_index]
-    assert opening_token.nesting == 1, "Cant find closing token for non opening token"
-    for i in range(opening_index + 1, len(tokens)):
-        closing_candidate = tokens[i]
-        if closing_candidate.level == opening_token.level:
-            return i
-    raise ValueError("Invalid token list. Closing token not found.")
+def _list_item_renderer(
+    node: RenderTreeNode,
+    renderer_funcs: Mapping[str, RendererFunc],
+    options: Mapping[str, Any],
+    env: MutableMapping,
+) -> str:
+    classes = node.attrs.get("class")
+    if classes is None or "task-list-item" not in classes:
+        return DEFAULT_RENDERER_FUNCS["list_item"](node, renderer_funcs, options, env)
+
+    # Tasklists extension makes a bit weird token stream where
+    # tasks are annotated by html. We need to remove the HTML.
+    paragraph_node = node.children[0]
+    inline_node = paragraph_node.children[0]
+    assert inline_node.type == "inline"
+    assert inline_node.children, "inline token must have children"
+    html_inline_node = inline_node.children[0]
+    assert 'class="task-list-item-checkbox"' in html_inline_node.content
+
+    # This is naughty, shouldn't mutate and rely on `.remove` here
+    inline_node.children.remove(html_inline_node)
+
+    checkmark = "x" if 'checked="checked"' in html_inline_node.content else " "
+
+    text = DEFAULT_RENDERER_FUNCS["list_item"](node, renderer_funcs, options, env)
+    # Strip leading space chars (numeric representations)
+    text = re.sub(r"^(&#32;)+", "", text)
+    return f"[{checkmark}] {text}"
+
+
+RENDERER_FUNCS = {"s": _strikethrough_renderer, "list_item": _list_item_renderer}

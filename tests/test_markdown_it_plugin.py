0314b4f2017c81c7147290480f528ed8767ae031
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2024-12-11 18:33:04 +0200

    Add tests

--- tests/test_markdown_it_plugin.py
+++ tests/test_markdown_it_plugin.py
@@ -1,14 +1,16 @@
+from pathlib import Path
+
 from markdown_it import MarkdownIt
+from markdown_it.utils import read_fixture_file
+import pytest
 
 from mdformat_gfm._mdit_gfm_autolink_plugin import gfm_autolink_plugin
 
+FIXTURE_PATH = Path(__file__).parent / "data" / "gfm_autolink.md"
 
-def test_gfm_autolink():
-    mdit = MarkdownIt()
-    mdit.use(gfm_autolink_plugin)
-    text = "GFM autolink www.commonmark.org"
-    html = mdit.render(text)
-    assert (
-        html
-        == '<p>GFM autolink <a href="http://www.commonmark.org">www.commonmark.org</a></p>\n'  # noqa: E501
-    )
+
+@pytest.mark.parametrize("line,title,md,expected_html", read_fixture_file(FIXTURE_PATH))
+def test_gfm_autolink(line, title, md, expected_html):
+    mdit = MarkdownIt().use(gfm_autolink_plugin)
+    text = mdit.render(md)
+    assert text.rstrip() == expected_html.rstrip()
